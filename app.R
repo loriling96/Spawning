43d15b500649fdb14526ca0a1687251e45def0c0
Lorraine Ling <lorraineling@Lorraines-MacBook-Pro.local>
2017-03-07 01:06:30 -0800

    using SpawningData Google spreadsheet which is different format than previous spawning log

--- app.R
+++ app.R
@@ -1,4 +1,4 @@
-##shiny app for spawning calendar
+##shiny app for spawning data calendar plots
 
 library(shiny)
 library(ggplot2)
@@ -11,9 +11,9 @@ ui <- fluidPage(
   tags$h2("Frequency of Aiptasia spawning in Pringle Lab"),
   column(3,
       dateRangeInput(inputId = "daterange", label = "Enter a date range", 
-                 format = "yyyy-mm-dd", start = "2016-01-01", end = "2016-12-31"),
+                 format = "yyyy-mm-dd", start = "2016-01-01", end = "2017-03-01"),
       checkboxGroupInput(inputId = "target", label = "Choose any/all incubators:", 
-                choices = c("Cabinet", "New", "Newest","Old", "Ron"))
+                choices = c("Blue (Cabinet)", "Red (New)", "Green (Newest)","Orange (Old)", "Yellow (Ron)"))
         ),
   column(9,
          plotOutput(outputId = "spawnplot1"),
@@ -21,29 +21,33 @@ ui <- fluidPage(
   
 )
 
-data_long <- readRDS("Data/2016spawncounts.rds")
+SpawnDF <- read.csv(file="Data/SpawningData20170306.csv", header = TRUE)
+SpawnDF$Spawn.Date=as.Date(SpawnDF$Spawn.Date, format = "%m/%d/%y")
 
 server <- function(input, output){
-  output$spawnplot1 <-renderPlot({
-        data_long %>% 
-          filter(Date > input$daterange[1] & Date < input$daterange[2]) %>%
-            filter(Incubator %in% input$target) %>%
-              ggplot( aes(x=Day.of.Cycle, y=Count, fill=Spawn_type)) +
-              geom_bar(stat = "identity") +
-              scale_x_continuous(breaks = c(1:30)) + 
-              #scale_y_continuous(breaks = seq(0, 15, by =1)) +  #kept adjusting range for different incubators
-              ggtitle("Spawning Frequency") +
-              theme_bw()
-  })
-  
-  output$spawnplot2 <- renderPlot({
-    data_long %>% 
-      filter(Date > input$daterange[1] & Date < input$daterange[2]) %>% 
-      filter(Incubator %in% input$target) %>% 
-        ggplot( aes(x=Date, y=Count, fill=Spawn_type)) +
+
+  output$spawnplot1 <- renderPlot({
+    SpawnDF %>% 
+      filter(Spawn.Date >= input$daterange[1] & Spawn.Date <= input$daterange[2]) %>% 
+      filter(Incubator %in% input$target) %>%
+      group_by(Spawn.Date) %>% 
+      count(Spawn.Type) %>%
+        ggplot( aes(x=Spawn.Date, y=n, fill=Spawn.Type)) +
         geom_bar(stat = "identity") +
-        ggtitle("Spawning Frequency") +
+        ggtitle("Spawning Frequency by calendar dates") + ylab("Counts")+
         theme_bw()
+  })
+  output$spawnplot2 <-renderPlot({
+    SpawnDF %>% 
+      filter(Spawn.Date >= input$daterange[1] & Spawn.Date <= input$daterange[2]) %>%
+      filter(Incubator %in% input$target) %>%
+      group_by(Day.of.Cycle) %>% 
+      count(Spawn.Type) %>%
+      ggplot( aes(x=Day.of.Cycle, y=n, fill=Spawn.Type)) +
+      geom_bar(stat = "identity") +
+      scale_x_continuous(breaks = c(1:30)) + 
+      ggtitle("Spawning Frequency by Lunar Cycle") + ylab("Counts")+
+      theme_bw()
   })
 }
 
