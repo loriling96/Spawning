77b7dd2c2dc907529209b684f5f1da9b81658009
Lorraine Ling <lorraineling@Lorraines-MacBook-Pro.local>
2017-03-06 21:29:12 -0800

    commented all the filter and plotting code because that module is now in the shiny app. Spawning Data get counts and reshape DF to long format

--- plot_spawning.R
+++ plot_spawning.R
@@ -5,44 +5,73 @@ library(dplyr)
 library(magrittr)
 library(tidyr)
 
+# Using updated Spawning Data retrieved 2017-03-06 ---------------------------------------------
+SpawnDF=read.csv(file="./Data/SpawningData20170306.csv")
+
+levels(SpawnDF$Spawn.Type)
+
+#make sure Spawn.Dates are date class
+SpawnDF$Spawn.Date=as.Date(SpawnDF$Spawn.Date, format = "%m/%d/%y")
+
+#count spawning events
+All_count<- SpawnDF %>% 
+  group_by(Spawn.Date, Day.of.Cycle, Incubator, Total...of.tanks) %>% 
+  count(Spawn.Type) %>% 
+  as.data.frame()
+
+colnames(All_count)[colnames(All_count)=="n"] <- "Count"
+
+#calculate incubator efficiency Count/Tanks
+All_count<- All_count %>% mutate(norm.Count = Count/ Total...of.tanks)
+
+saveRDS(All_count, file = "./Data/SpawnCounts20170306.rds")
 
 # MERGE ALL INCUBATOR FORMATTED DATA INTO SUPER DF ------------------------
 
-Ron_count=read.csv(file = "../Spawning/formattedData/Ron_spawncounts.csv", header = TRUE)
-New_count=read.csv(file="../Spawning/formattedData/NewIncubator_spawncounts.csv", header = TRUE)
-Newest_count=read.csv(file = "../Spawning/formattedData/NewestIncubator_spawncounts.csv", header= TRUE)
-Old_count=read.csv(file = "../Spawning/formattedData/OldIncubator_spawncounts.csv", header= TRUE)
-Cabinet_count=read.csv(file = "../Spawning/formattedData/Cabinet_spawncounts.csv", header= TRUE)
-
-ALL_count=dplyr::bind_rows(Cabinet_count, New_count, Newest_count, Old_count, Ron_count)
-
-data_long = ALL_count %>% 
-  gather(key= "Spawn_type", value = "count", 3:7)
-
-data_long$Date = as.Date(data_long$Date)
-
-
-# plots in lunar cycle days -----------------------------------------------
-
-data_long %>% 
-  filter(Date > '2016-12-01' & Date < '2016-12-31') %>% 
-  ggplot( aes(x=Day.of.Cycle, y=Count, fill=Spawn_type)) +
-  geom_bar(stat = "identity") +
-  scale_x_continuous(breaks = c(1:30)) + 
-  scale_y_continuous(breaks = seq(0, 15, by =1)) +  #kept adjusting range for different incubators
-  ggtitle("Spawning Frequency") +
-  theme_bw()
+# Cabinet_count=read.csv(file = "./Data/Cabinet_spawncounts.csv")
+# New_count=read.csv(file="./Data/NewIncubator_spawncounts.csv")
+# Newest_count=read.csv(file = "./Data/NewestIncubator_spawncounts.csv")
+# Old_count=read.csv(file = "./Data/OldIncubator_spawncounts.csv")
+# Ron_count=read.csv(file = "./Data/Ron_spawncounts.csv")
+# 
+# #I don't understand why the read.csv function reads date in YYYY-MM-DD format for Ron and Cabinet but not the other three
+# #format into Date class
+# Old_count$Date=as.Date(Old_count$Date, format = "%m/%d/%y")
+# New_count$Date=as.Date(New_count$Date, format = "%m/%d/%y")
+# Newest_count$Date=as.Date(Newest_count$Date, format = "%m/%d/%y")
+# Ron_count$Date=as.Date(Ron_count$Date)
+# Cabinet_count$Date=as.Date(Cabinet_count$Date)
+# 
+# ALL_count=dplyr::bind_rows(Cabinet_count, New_count, Newest_count, Old_count, Ron_count)
+# ALL_count$Incubator=as.factor(ALL_count$Incubator) 
+# 
+# data_long = ALL_count %>% 
+#   gather(key= "Spawn_type", value = "Count", 3:7)
+# data_long$Spawn_type=as.factor(data_long$Spawn_type) 
+# levels(data_long$Spawn_type) = c("DisEgg", "Eggs", "Sperm", "Embryos", "Larvae")
 
 
-
-# plots in calendar days --------------------------------------------------
-data_long %>% 
-  filter(Date > '2016-12-01' & Date < '2016-12-31') %>%
-  ggplot(data=data_long, aes(x=Date, y=Count, fill=Spawn_type)) + 
-  geom_bar(stat = "identity") +
-  scale_x_date(date_breaks="month", date_labels=("%b"))+
-  ggtitle("Spawning Frequency") +
-  theme_bw()
+# # plots in lunar cycle days -----------------------------------------------
+# 
+# data_long %>% 
+#   filter(Date > '2016-01-01' & Date < '2016-12-31' & Incubator == "Old") %>% 
+#   ggplot( aes(x=Date, y=Count, fill=Spawn_type)) +
+#   geom_bar(stat = "identity") +
+#   #scale_x_continuous(breaks = c(1:30)) + 
+#   #scale_y_continuous(breaks = seq(0, 15, by =1)) +  #kept adjusting range for different incubators
+#   ggtitle("Spawning Frequency") +
+#   theme_bw()
+# 
+# 
+# 
+# # plots in calendar days --------------------------------------------------
+# data_long %>% 
+#   filter(Date > '2016-12-01' & Date < '2016-12-31') %>%
+#   ggplot(data=data_long, aes(x=Date, y=Count, fill=Spawn_type)) + 
+#   geom_bar(stat = "identity") +
+#   scale_x_date(date_breaks="month", date_labels=("%b"))+
+#   ggtitle("Spawning Frequency") +
+#   theme_bw()
 
 
   
